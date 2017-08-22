cb126e17d80eef667d703552badcdfddb6f79f8e
myemail <myemail@emal.com>
2017-08-22 14:42:00 +0800

    Initial Commit

---
+++ bearTagTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bearTagTool
+//
+//  Created by 黄家树 on 2017/8/22.
+//  Copyright © 2017年 com.id-bear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
